5e63f05c7b9800d25f7481bd5ab85dccfd0f18e5
LMBSHKA <makstanc2013@yandex.ru>
2026-05-25 15:55:06 +0500

    fix: add secure for jwt

--- API/API/Controllers/AuthController.cs
+++ API/API/Controllers/AuthController.cs
@@ -43,7 +43,7 @@ public class AuthController(
         Response.Cookies.Append(_jwtOptions.CookieName, authResult.Token.AccessToken, new CookieOptions
         {
             HttpOnly = true,
-            Secure = Request.IsHttps,
+            Secure = true,
             SameSite = SameSiteMode.None,
             Expires = authResult.Token.ExpiresAtUtc,
             IsEssential = true
